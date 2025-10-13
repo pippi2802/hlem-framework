49bfebcd68545bb172a878d8da8aa5731d624e8e
silvia <silvia.brighi02@gmail.com>
2025-10-13 17:05:33 +0200

    remove: unwanted files

--- src/hlem_framework/bpic2017_analysis/preprocessing.py
+++ src/hlem_framework/bpic2017_analysis/preprocessing.py
@@ -20,3 +20,14 @@ def get_resources(log, to_exclude=[]):
                     resouces_list.append(event['org:resource'])
 
     return resouces_list
+
+def filter_uncomplete_traces(log):
+    """
+    Remove all incomplete traces from the event log.
+
+    :param log: The input event log.
+    :return: The modified event log with incomplete traces removed.
+    """
+    logging.info("Filter incomplete traces")
+    complete_log = pm4py.filter_traces(lambda t: all(event['lifecycle:transition'] == 'complete' for event in t), log)
+    return complete_log
